09fb9a5e7d40a03af9586bb582562c402af04991
mmartyna123 <mmartyna.stasiak@gmail.com>
2025-01-24 22:51:14 +0100

    new symmetry check

--- LightSide/finalMetrics.py
+++ LightSide/finalMetrics.py
@@ -10,7 +10,7 @@ from scipy.stats import wasserstein_distance_nd
 from scipy.sparse import lil_matrix
 import ot
 from scipy.sparse import csr_matrix
-
+import math 
 
 # Function to calculate total variation distance
 def total_variation_distance(voxel1, voxel2):
@@ -82,7 +82,9 @@ def wasserstein_distance_3d_optimized(grid1, grid2, reg=0.1, threshold=0.01, dow
     # Normalize Wasserstein distance to obtain similarity
     similarity = 1 - (wasserstein_distance / max_distance)
 
-    similarity = round(similarity, 5)
+    similarity *= 1000
+    similarity = math.floor(similarity)
+    similarity /= 1000
 
     return similarity
 
@@ -139,7 +141,9 @@ def wasserstein_distance_3d_optimized_normalized(grid1, grid2, reg=0.1, threshol
     # NORMALIZATION -- IS THERE BETTER APPROACH ??
     similarity = 1 - (wasserstein_distance / max_distance)
     
-    similarity = round(similarity, 5)
+    similarity *= 1000
+    similarity = math.floor(similarity)
+    similarity /= 1000
     
     return similarity
 
